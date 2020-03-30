7430d4bde81acb02d0f38a66e8cf68b517e56a82
Michiel van der Velde <michiel@michielvdvelde.nl>
2020-03-30 15:22:29 +0200

    chore(release): v0.0.1

---
+++ README.md
@@ -0,0 +1,80 @@
+# Eve Single Sign On
+
+[Eve Online](https://eveonline.com) Single Sign On (SSO) module for node.js.
+Once the user/character is authenticated, you can use the access token to make
+authenticated requests to [ESI](https://docs.esi.evetech.net/docs/esi_introduction.html)
+(not included in this module).
+
+Supports defining scopes as well. For now, please refer to the source code
+or the example for more information on how to use this module (documentation
+is forthcoming).
+
+Before using the module you must create an application in the
+[Eve Online developers section](https://developers.eveonline.com/). This will
+give you the required client ID and secret.
+
+[See the developer documentation for more information](https://docs.esi.evetech.net/docs/sso/).
+
+## Example
+
+A small example using koa.
+
+```ts
+import SingleSignOn from './index'
+import Koa from 'koa'
+import Router from 'koa-router'
+
+// Get the client ID and secret from the Eve developers section
+const CLIENT_ID = 'your client id'
+const SECRET = 'your secret'
+// The callback URI as defined in the application in the developers section
+const CALLBACK_URI = 'http://localhost:3001/sso'
+
+const sso = new SingleSignOn(CLIENT_ID, SECRET, CALLBACK_URI, {
+  // both options are optional
+  endpoint: 'https://login.eveonline.com' // defaults to this
+  userAgent: 'my-user-agent'
+})
+
+const app = new Koa()
+const router = new Router()
+
+// Show a login redirect link
+router.get('/login', async ctx => {
+  // The first argument is an optional state, which you can verify in the callback
+  // The second argument is a space-delimited string or string array of scopes to request
+  ctx.body = `<a href="${sso.getRedirectUrl('my-state')}">Login to Eve Online</a>`
+})
+
+// Handle the SSO callback (this route is the CALLBACK_URI above)
+router.get('/sso', async ctx => {
+  // Get the one-time access code
+  const code: string = ctx.query.code
+  // NOTE: usually you'd want to validate the state (ctx.query.state) as well
+
+  // Swap the one-time code for an access token
+  const info = await sso.getAccessToken(code)
+  
+  // NOTE: this would be the place to verify the access token JWT (left out
+  // of this example for the sake of brevity)
+
+  // Usually you'd want to store the access token
+  // as well as the refresh token
+  console.log('info', info)
+  
+  // Do whatever, for example, redirect to user page
+  ctx.body = 'You are now authenticated!'
+})
+
+app.use(router.middleware())
+app.listen(3001, () => {
+  console.log('Server listening on port 3001')
+})
+
+```
+
+## License
+
+Copyright 2020 Michiel van der Velde.
+
+This software is licensed under [the MIT License](LICENSE).
---
+++ src/index.ts
@@ -0,0 +1,110 @@
+'use strict'
+
+import { stringify } from 'querystring'
+import { parse } from 'url'
+
+import bent from 'bent'
+import formUrlEncoded from 'form-urlencoded'
+
+const { name, version, homepage } = require('../package')
+
+export interface SingleSignOnOptions {
+  endpoint?: string
+  userAgent?: string
+}
+
+export default class SingleSignOn {
+  public readonly clientId: string
+  public readonly callbackUri: string
+  public readonly endpoint: string
+  public readonly userAgent: string
+
+  #secretKey: string
+  #authorization: string
+  #host: string
+  #request: bent.RequestFunction<bent.ValidResponse>
+
+  public constructor (
+    clientId: string,
+    secretKey: string,
+    callbackUri: string,
+    opts: SingleSignOnOptions = {}
+  ) {
+    this.clientId = clientId
+    this.#secretKey = secretKey
+    this.callbackUri = callbackUri
+
+    this.endpoint = opts.endpoint || 'https://login.eveonline.com'
+    this.userAgent = opts.userAgent || `${name}@${version} - nodejs@${process.version} - ${homepage}`
+
+    this.#authorization = Buffer.from(`${this.clientId}:${this.#secretKey}`).toString('base64')
+    this.#host = parse(this.endpoint).hostname
+    this.#request = bent(this.endpoint, 'POST', 'json')
+  }
+
+  /**
+   * Get a redirect url.
+   * @param  state  State string
+   * @param  scopes Scopes to request
+   * @return        Redirect url
+   */
+  public getRedirectUrl (state?: string, scopes?: string | string[]) {
+    const query: any = {
+      response_type: 'code',
+      redirect_uri: this.callbackUri,
+      client_id: this.clientId,
+      scope: Array.isArray(scopes) ? scopes.join(' ') : (scopes || '')
+    }
+
+    if (state) {
+      query.state = state
+    }
+
+    return `${this.endpoint}/v2/oauth/authorize?${stringify(query)}`
+  }
+
+  /**
+   * Get an access token from the authorization code.
+   * @param  code Authorization code
+   * @return      An object containing, among other things, the access token
+   * and refresh token
+   */
+  public async getAccessToken (code: string) {
+    const payload = {
+      grant_type: 'authorization_code',
+      code
+    }
+
+    return this.#request('/v2/oauth/token', formUrlEncoded(payload), {
+      Host: this.#host,
+      Authorization: `Basic ${this.#authorization}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'User-Agent': this.userAgent
+    })
+  }
+
+  /**
+   * Get an access token from a refresh token.
+   * @param  refreshToken The refresh token
+   * @param  scopes       The scopes to request
+   * @return              An object containing, among other things, the access token
+   * and (potentially differing) refresh token
+   */
+  public async refreshToken (refreshToken: string, scopes?: string | string[]) {
+    const payload: any = {
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken
+    }
+
+    if (scopes) {
+      payload.scope = Array.isArray(scopes) ? scopes.join(' ') : scopes
+    }
+
+    return this.#request('/v2/oauth/token', formUrlEncoded(payload), {
+      Host: this.#host,
+      Authorization: `Basic ${this.#authorization}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'User-Agent': this.userAgent
+    })
+  }
+}
